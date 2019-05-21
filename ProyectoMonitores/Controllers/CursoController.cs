7d666012c37d58bc6a1cd10be1c578ccb717f034
Duvan Giraldo Duque <duvan.giraldo@upb.edu.co>
2019-05-21 11:40:43 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoMonitores/Controllers/CursoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoMonitores.Models;
+
+namespace ProyectoMonitores.Controllers
+{
+    public class CursoController : Controller
+    {
+
+        private BaseMonitorEntities db = new BaseMonitorEntities();
+        // GET: Curso
+        public ActionResult Index()
+        {
+
+            return View(db.Curso.ToList());
+        }
+
+        public ActionResult CrearCurso(Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Curso.Add(curso);
+                db.SaveChanges();
+
+                
+            }
+           
+
+           
+
+            return View(curso);
+        }
+
+        public ActionResult EncontrarCurso(int id)
+        {
+
+            Curso curso = db.Curso.Find(id);
+
+            return View(curso);
+        }
+
+        public ActionResult EliminarCurso(int id)
+        {
+
+            try
+            {
+                Curso curso = db.Curso.Find(id);
+                db.Curso.Remove(curso);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        
+    }
+}
---
+++ ProyectoMonitores/Controllers/MonitorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMonitores.Controllers
+{
+    public class MonitorController : Controller
+    {
+        // GET: Monitor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+       
+    }
+}
