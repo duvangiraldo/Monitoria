d711a4c253ac5df951710f273e6300c8ff81de69
Duvan Giraldo Duque <duvan.giraldo@upb.edu.co>
2019-05-21 14:46:48 -0500

    monitores

---
+++ ProyectoMonitores/Controllers/AsesoriaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoMonitores.Models;
+
+namespace ProyectoMonitores.Controllers
+{
+    public class AsesoriaController : Controller
+    {
+        public BaseMonitorEntities db = new BaseMonitorEntities();
+        // GET: Asesoria
+        public ActionResult Index()
+        {
+            return View(db.Asesoria.ToList());
+        }
+
+        public ActionResult CrearAsesoria(Asesoria asesoria)
+        {
+
+            db.Asesoria.Add(asesoria);
+            db.SaveChanges();
+
+            
+            ViewBag.idCurso = new SelectList(db.Curso, "idCurso", "nombreCurso", asesoria.idCurso);
+            ViewBag.idMonitor = new SelectList(db.Monitor, "idMonitor", "nombreMonitor", asesoria.idMonitor);
+
+
+            return View();
+        }
+
+        public ActionResult EncontrarAsesoria(int id)
+        {
+
+            IEnumerable<Asesoria> asesorias=db.Curso.Where(x= > XmlSiteMapProvider.)
+
+            return View(curso);
+        }
+    }
+}
--- ProyectoMonitores/Controllers/MonitorController.cs
+++ ProyectoMonitores/Controllers/MonitorController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProyectoMonitores.Models;
 
 namespace ProyectoMonitores.Controllers
 {
     public class MonitorController : Controller
     {
         // GET: Monitor
+        private BaseMonitorEntities db = new BaseMonitorEntities();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Monitor.ToList());
+        }
+
+        public ActionResult CrearMonitor(Monitor monitor)
+        {
+
+            db.Monitor.Add(monitor);
+            db.SaveChanges();
+
+            ViewBag.idCurso = new SelectList(db.Curso, "idCurso", "nombreCurso", monitor.idCurso);
+            return View(monitor);
         }
 
        
